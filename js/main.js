3aa3ae0d2151c51d48465fc0be7d8c6ea09b5578
colorkid <colorkid@yandex.ru>
2016-11-14 10:34:01 +0300

    wave finish

--- js/main.js
+++ js/main.js
@@ -211,6 +211,22 @@ var body = document.querySelector("body");
 				popUpDescription.className = "popup-card__description";
 				popUpDescription.innerHTML = description;
 				popUpCardCard.appendChild(popUpDescription);
+				var parent, ink, d, x, y;
+				$(".wave").mousedown(function(e){
+					parent = $(this).parent();
+					if(parent.find(".ink").length == 0)
+						parent.prepend("<span class='ink'></span>");				        
+						ink = parent.find(".ink");		   
+						ink.removeClass("animate");    
+						if(!ink.height() && !ink.width())
+							{
+							    d = Math.max(parent.outerWidth(), parent.outerHeight());
+							    ink.css({height: d, width: d});
+						}
+					x = e.pageX - parent.offset().left - ink.width()/2;
+					y = e.pageY - parent.offset().top - ink.height()/2;	     
+					ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+				});
 			});
 		}
 
